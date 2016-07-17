b1dcb3ad326f6cc796b13b54ec8e101db79d964d
tcltien <atien.lh@gmail.com>
2016-07-17 23:13:36 +0700

    Add ReadMe

---
+++ Yelp/SelectCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelectCell.swift
+//  Yelp
+//
+//  Created by Le Huynh Anh Tien on 7/17/16.
+//  Copyright © 2016 Tien Le. All rights reserved.
+//
+
+import UIKit
+
+class SelectCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Yelp/YelpClient.swift
@@ -0,0 +1,9 @@
+//
+//  YelpClient.swift
+//  Yelp
+//
+//  Created by Le Huynh Anh Tien on 7/16/16.
+//  Copyright © 2016 Tien Le. All rights reserved.
+//
+
+import Foundation
